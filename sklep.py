0c30e324b853eebe9892b56e3aa73abb3364db24
pziecina <pziecin@gmail.com>
2020-05-07 14:29:02 +0200

    chart

--- sklep.py
+++ sklep.py
@@ -1,15 +1,31 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-kasy = int(input("Podaj liczbe kas: "))
-przedkasy = int(input("Podaj liczbe miejsc przed kasa: "))
-intensywnosc = int(input("Podaj intensywnosc naplywu klientow: "))
-obsluga = int(input("Podaj sredni czas obslugi klienta: "))
+bufor = 2 #int(input("Podaj wielkosc bufora: "))
+intensywnosc =2 #int(input("Podaj intensywnosc naplywu pakietow: "))
+obsluga =2 #int(input("Podaj sredni czas obslugi pakietu: "))
 
-a = kasy+przedkasy
+a = bufor
 
 matrix = np.zeros((a, a))
 
 itera = 0
+
+
+lamba = np.arange(0.5, 6.1, 0.1)
+mi = 8
+
+et = lamba/mi
+print(et)
+
+eb = 1 / (mi - lamba)
+print(eb)
+
+plt.plot(lamba, eb)
+plt.xlim([0,6.5])
+plt.ylim([0.1,0.55])
+plt.show()
+
 
 for i in range(0,a):
     for j in range(0,a):
@@ -20,17 +36,18 @@ for i in range(0,a):
             matrix[i][j] = 1
             continue
         if(j==i-1):
-            if(itera == kasy):
-                itera = kasy
+            if(itera == bufor):
+                itera = bufor
             else:
                 itera= itera + 1
-            matrix[i][j] = -intensywnosc/(obsluga*itera)
+            matrix[i][j] = -intensywnosc/obsluga
             continue
         matrix[i][j] = 0
 
 # print(matrix)
 
 # print(np.linalg.det(matrix))
+print(matrix)
 
 matrixinv = np.linalg.inv(matrix)
 
